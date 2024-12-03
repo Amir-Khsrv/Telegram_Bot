3e90c5cac69d761cce049f99749182e8831c790e
Amir-Khsrv <145700200+Amir-Khsrv@users.noreply.github.com>
2024-12-03 23:12:36 +0330

    Update TalleyBot.py

--- TalleyBot.py
+++ TalleyBot.py
@@ -111,3 +111,18 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+
+
+
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Default to port 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
